ce0beb47d70049da5d9a61ea96cc93b1ddc8392e
striker1826 <alstjq123579@daum.net>
2024-03-11 18:12:41 +0900

    feat: 뼈대 로직 구상 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { styled } from "styled-components";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+
+const CodeContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 50%;
+  height: 400px;
+`;
+
+const TestResultDiv = styled.div`
+  // width: 400px;
+  // height: 400px;
+`;
 
 function App() {
+  const [value, setValue] = useState(`function solution(a, b) {
+
+  }`);
+  const [testResult, setTestResult] = useState([]);
+
+  const [question, setQuestion] = useState();
+
+  const onChange = useCallback((val) => {
+    setValue(val);
+  }, []);
+
+  useEffect(() => {
+    const getQuestion = async () => {
+      const response = await axios.get("http://localhost:8000/question");
+      const question = response.data.question;
+      setQuestion(question);
+    };
+
+    getQuestion();
+  }, []);
+
+  const submit = async () => {
+    const response = await axios.post("http://localhost:8000/question/grading/2", {
+      code: value,
+    });
+    const testResult = response.data;
+    console.log(testResult);
+    setTestResult((prev) => {
+      return [...testResult];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CodeContainer>
+        <StyledTextarea value={question}></StyledTextarea>
+        <CodeMirror
+          value={value}
+          height="200px"
+          width="500px"
+          extensions={[javascript({ jsx: true })]}
+          onChange={onChange}
+        />
+      </CodeContainer>
+      <button onClick={submit}>제출</button>
+      {testResult.map((result, i) => {
+        return (
+          <TestResultDiv key={i}>
+            <h3>Test {i + 1}</h3>
+            <p>Result: {result.result}</p>
+          </TestResultDiv>
+        );
+      })}
+    </>
   );
 }
 
