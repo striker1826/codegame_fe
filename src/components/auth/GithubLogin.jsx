3cba04c0a6bf6321062892b4cf3003f2e7f02373
striker1826 <alstjq123579@daum.net>
2024-03-14 19:15:53 +0900

    feat

--- src/components/auth/GithubLogin.jsx
+++ src/components/auth/GithubLogin.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { styled } from "styled-components";
 
 export const GithubLogin = () => {
-  const githubLoginURL = `https://github.com/login/oauth/authorize?client_id=f43914b1d8d9e9a27d0f&redirect_uri=http://localhost:3000/github`;
+  const githubLoginURL = `https://github.com/login/oauth/authorize?client_id=f43914b1d8d9e9a27d0f&redirect_uri=https://algorithmcodegame.netlify.app/github`;
   const handleLogin = () => {
     window.location.href = githubLoginURL;
   };
