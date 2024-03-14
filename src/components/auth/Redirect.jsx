34517253818b69eea34a3ea5d26b0c291b940776
striker1826 <alstjq123579@daum.net>
2024-03-14 19:36:40 +0900

    Feat

--- src/components/auth/Redirect.jsx
+++ src/components/auth/Redirect.jsx
@@ -4,14 +4,12 @@ import axios from "axios";
 
 export const Redirect = () => {
   const params = new URLSearchParams(window.location.search);
-
+  console.log("redirect");
   useEffect(() => {
     const sendCode = async () => {
       const code = params.get("code");
-      console.log(code);
       const res = await axios.get(`https://minseob-codegame.koyeb.app/api/auth/github?code=${code}`);
       const access_token = res.data;
-      console.log(access_token);
       axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
       window.location.href = "/lobby";
     };
