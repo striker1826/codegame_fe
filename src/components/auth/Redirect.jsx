038d1046adacf8c48b6303c49fcb0a62320f4d81
striker1826 <alstjq123579@daum.net>
2024-03-25 16:02:57 +0900

    fix: redirect Component error fixed

--- src/components/auth/Redirect.jsx
+++ src/components/auth/Redirect.jsx
@@ -13,16 +13,17 @@ export const Redirect = () => {
       const code = params.get("code");
       try {
         const res = await axios.get(`${BASE_URL}/api/auth/github?code=${code}`);
+
+        const access_token = res.data;
+        localStorage.setItem("access_token", access_token);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+        window.location.href = "/lobby";
       } catch (err) {
         const status = err.response.status;
         if (status === 429) {
           alert("너무 많은 요청을 한꺼번에 보냈습니다. 잠시 후 다시 시도해주세요.");
         }
       }
-      const access_token = res.data;
-      localStorage.setItem("access_token", access_token);
-      axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
-      window.location.href = "/lobby";
     };
     sendCode();
   }, []);
